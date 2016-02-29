1f632240f564b409ffaebb37f6085f85089acdfa
Bruce Williams <brwcodes@gmail.com>
2016-02-29 11:37:00 -0800

    Build collection navigation

--- lib/helpers.rb
+++ lib/helpers.rb
@@ -1,3 +1,6 @@
-require_relative './partials_helper'
+Dir[File.expand_path('../helpers/**/*.rb', __FILE__)].each do |filename|
+  require filename
+end
 
 include PartialsHelper
+include CollectionHelper
