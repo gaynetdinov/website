d177b4879dc624c2a5e1b02efc32a452f9aad4a5
Lynn Wallenstein <lynn.wallenstein@gmail.com>
2016-03-02 20:32:50 -0800

    Main navigation on mobile

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,14 @@
 import hello from './hello';
 
+import $ from '../node_modules/jquery/dist/jquery.js';
+
 window.onload = () => {
-  hello();
+  $( "body" ).removeClass( "preload" );
+  $( "html" ).removeClass( "no-js" );
+
+
+  $(".js-mobile-nav").click(function() {
+      $("body").toggleClass("mobile-nav-open");
+  });
+
 }
